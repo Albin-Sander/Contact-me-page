fbaadeff477802ac790eec7e3d4eae07e5d02916
Albin Sander <51294735+Albin-Sander@users.noreply.github.com>
2021-04-30 11:43:49 +0200

    Added image switching depending on light/dark mode.

--- src/components/Github.vue
+++ src/components/Github.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="flex flex-row justify-around">
-    <a href="https://github.com/Albin-Sander">
-      <img
-        src="../assets/github-light.png"
-        class="md:h-12 md:w-12 h-8 w-8 animate-fade-in-down hover:animate-bounce"
-      />
+    <a href="https://github.com/Albin-Sander" class="">
+      <picture>
+        <source
+          srcset="../assets/github-light.png"
+          media="(prefers-color-scheme: dark)"
+        />
+        <img
+          src="../assets/github.png"
+          alt="moving car"
+          class="md:h-12 md:w-12 h-8 w-8 animate-fade-in-down dark:text-white"
+        />
+      </picture>
     </a>
     <a href="mailto:albinsander17@gmail.com" class="animate-fade-in-down">
-      <MailIcon class="md:h-12 md:w-12 h-8 w-8 text-white" />
+      <MailIcon class="md:h-12 md:w-12 h-8 w-8 dark:text-white" />
     </a>
   </div>
 </template>
