ca5a8dc29332909cfd55c4a2e6f961d1a85547e1
Albin Sander <51294735+Albin-Sander@users.noreply.github.com>
2023-01-04 14:45:46 +0100

    Removed card

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,11 @@
     <div class="flex-col">
       <Name />
       <Github />
-      <div class="pl-8 pr-8">
-        <Card class="mt-20" />
-      </div>
     </div>
   </div>
 </template>
 
 <script>
-import Card from "./components/Card.vue";
 import Name from "./components/Name";
 import Github from "./components/Github";
 
@@ -20,7 +16,6 @@ export default {
   components: {
     Name,
     Github,
-    Card,
   },
   created() {
     document.title = "Albin Sander";
